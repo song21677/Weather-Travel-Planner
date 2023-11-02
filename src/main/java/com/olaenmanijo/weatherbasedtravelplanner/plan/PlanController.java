2dea71dbc4ca68e3c62eeb62b38537b742b8cd77
song21677@naver.com <song21677@naver.com>
2023-11-02 10:58:20 +0900

    일정 페이지 Controller

---
+++ src/main/java/com/olaenmanijo/weatherbasedtravelplanner/plan/PlanController.java
@@ -0,0 +1,57 @@
+package com.olaenmanijo.weatherbasedtravelplanner.plan;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.olaenmanijo.weatherbasedtravelplanner.tourapi.dao.PlaceDAO;
+import com.olaenmanijo.weatherbasedtravelplanner.tourapi.domain.Place;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class PlanController {
+	@Autowired
+	PlaceDAO placeDAO;
+
+	@GetMapping("/plan")
+	public String plan() {
+		return "planPage/plan";
+	}
+	
+	@GetMapping("/planForm")
+	public String planHTML() {
+		return "planPage/planForm";
+	}
+	
+	@GetMapping("/search")
+	public String search(@RequestParam(required = false) String area, @RequestParam(required = false) String category, Model model) throws IOException, URISyntaxException {
+		if (area != null && category != null) {
+			 
+			Map<String, String> paramMap = new HashMap<>();
+			if (area.equals("6")) area="부산";
+			if (category.equals("39")) category="음식점";
+	        paramMap.put("category", category);
+	        paramMap.put("location", area);
+			
+	        ArrayList<Place> places = (ArrayList<Place>) placeDAO.selectByNameAndCategory(paramMap);
+	        model.addAttribute("places", places);
+	        
+		}
+		return "planPage/searchForm";
+	}
+}
+ 
+
