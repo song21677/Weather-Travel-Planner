0c0e5d68bc697741e8346f6d1a8768265ab3621b
song21677@naver.com <song21677@naver.com>
2023-11-02 10:53:01 +0900

    feat: 장소 정보 저장 DAO

---
+++ src/main/java/com/olaenmanijo/weatherbasedtravelplanner/tourapi/dao/PlaceDAO.java
@@ -0,0 +1,27 @@
+package com.olaenmanijo.weatherbasedtravelplanner.tourapi.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.olaenmanijo.weatherbasedtravelplanner.tourapi.domain.Place;
+import com.olaenmanijo.weatherbasedtravelplanner.tourapi.dto.Item;
+
+@Repository
+public class PlaceDAO {
+
+	@Autowired
+	SqlSessionTemplate session;  
+	
+	public int insertPlace(Item place) {
+		return session.insert("PlaceMapper.insertPlace", place);
+	}
+
+	public List<Place> selectByNameAndCategory(Map<String, String> paramMap) {
+		List<Place> list = session.selectList("PlaceMapper.selectByNameAndCategory", paramMap);
+		return list;
+	}
+}
