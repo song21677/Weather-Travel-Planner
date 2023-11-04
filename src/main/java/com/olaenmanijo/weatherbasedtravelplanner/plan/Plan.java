3b82afc6c562abb51afb9e9258f6c36384cb02dd
song21677@naver.com <song21677@naver.com>
2023-11-04 11:12:07 +0900

    feat: 추가한 일정 planForm 페이지로 전달

--- src/main/java/com/olaenmanijo/weatherbasedtravelplanner/plan/Plan.java
+++ src/main/java/com/olaenmanijo/weatherbasedtravelplanner/plan/Plan.java
@@ -1,15 +1,31 @@
 package com.olaenmanijo.weatherbasedtravelplanner.plan;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.Getter;
+import lombok.Setter;
+
 @Component
 @SessionScope
+@Getter
+@Setter
 public class Plan {
 
 	String daterange;
 	String title;
-	List<PlanDTO> planDTOs;
+	List<PlanDTO> planDTOs = new ArrayList<>();
+	
+	public Plan() {
+	}
+	
+	public void add(PlanDTO planDTO) {
+		planDTOs.add(planDTO);
+	}
 }
--- src/main/java/com/olaenmanijo/weatherbasedtravelplanner/plan/PlanDTO.java
+++ src/main/java/com/olaenmanijo/weatherbasedtravelplanner/plan/PlanDTO.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.context.annotation.SessionScope;
 
+import com.olaenmanijo.weatherbasedtravelplanner.tourapi.domain.Place;
+
 @Component
 @RequestScope
 public class PlanDTO {
 
-	String date;
-	String time;
-	String place;
+	private String date;
+	private String time;
+	private Place place;
+	
+	
+	public PlanDTO() {	
+	}
 	
 	public String getDate() {
 		return date;
@@ -24,10 +30,15 @@ public class PlanDTO {
 	public void setTime(String time) {
 		this.time = time;
 	}
-	public String getPlace() {
+	public Place getPlace() {
 		return place;
 	}
-	public void setPlace(String place) {
+	public void setPlace(Place place) {
 		this.place = place;
+	}
+	
+	@Override
+	public String toString() {
+		return "PlanDTO [date=" + date + ", time=" + time + ", place=" + place + "]";
 	}
 }
